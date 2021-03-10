53d93210cb4db23a47afe3e270e4fbb103e96a71
praveen Dhandapani <praveen.2471993@gmail.com>
2021-03-10 09:24:49 +0530

    Initial commit

---
+++ lib/src/main/java/hexad/LibraryApp.java
@@ -0,0 +1,19 @@
+package hexad;
+
+public class LibraryApp {
+
+    public static void main(String[] args) {
+        Library library = new Library();
+
+        //view the available Books
+        //library.viewBooks();
+
+        //User borrows a book
+        //user.borrow(bookObj);
+
+        //User Returns a book
+        //user.returns(bookObj);
+
+
+    }
+}
